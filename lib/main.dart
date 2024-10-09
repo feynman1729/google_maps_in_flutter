481d3cd8189f7dc616445f0804466ff700743e9a
feynman <feynman.diagram.511@gmail.com>
2024-10-09 23:15:28 +0900

    複数ルートを本実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
 import 'package:http/http.dart' as http;
 import 'dart:math';
 
